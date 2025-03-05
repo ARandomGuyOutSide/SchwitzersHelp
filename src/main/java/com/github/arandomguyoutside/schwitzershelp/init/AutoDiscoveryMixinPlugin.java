c9233a4daa2350a89dff058ee5d7bc7fe3880bb8
GitHub Action <action@github.com>
2025-03-05 09:36:35 +0000

    Initialize template repository proper
    Set package name to com.github.arandomguyoutside.schwitzershelp
    Set project name to SchwitzersHelp
    Set modid to schwitzershelp

--- src/main/java/com/github/arandomguyoutside/schwitzershelp/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/arandomguyoutside/schwitzershelp/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.arandomguyoutside.schwitzershelp.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
